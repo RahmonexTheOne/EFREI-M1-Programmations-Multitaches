eca6bbf58f515545ccf0fad15114a2ba846471ce
Bilel Rahmouni <iamrahmonex@gmail.com>
2024-10-24 16:56:21 +0200

    ADD : Add the fiels for TP2

---
+++ TP2/Docs_TP2/exemple2-Cr_ation_d_une_tache_dans_une_autre.ino
@@ -0,0 +1,68 @@
+//--Exemple 2 - Création d'une tâche dans une autre ------
+#include <Arduino_FreeRTOS.h>
+
+// define two Tasks 
+void Task1( void *pvParameters );
+void Task2( void *pvParameters );
+
+// the setup function runs once when you press reset or power the board
+void setup() {                
+              // initialize serial communication at 9600 bits per second:
+              Serial.begin(9600);         
+              while (!Serial) {
+                                 ; // wait for serial port to connect. Needed for native USB, on LEONARDO, MICRO, YUN, and other 32u4 based boards.
+                               }
+
+  // Now set up two Tasks to run independently wirh the same priority.
+  xTaskCreate(
+                Task1
+                ,  "Task 1"  // A name just for humans
+                ,  128  // This stack size can be checked & adjusted by reading the Stack Highwater
+                ,  NULL //Parameters for the task
+                ,  1  // Priority 1, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+                ,  NULL ); //Task Handle
+
+  vTaskStartScheduler(); //Start Scheduler
+}
+
+void loop()
+{
+  // Empty. Things are done in Tasks.
+}
+
+/*--------------------------------------------------*/
+/*---------------------- Tasks ---------------------*/
+/*--------------------------------------------------*/
+
+void Task1( void *pvParameters  )  // This is a Task 1.
+  {
+   // prints Tache 1 to the serial monitor
+    volatile uint32_t cnt ;
+    xTaskCreate(
+                Task2
+                ,  "Task 2" // A name just for humans
+                ,  128  // Stack size
+                ,  NULL //Parameters for the task
+                ,  1  // Priority
+                ,  NULL ); //Task Handle
+
+    for (;;) // A Task shall never return or exit.
+        {   
+          Serial.println(" Tache 1");
+          //delay(1000); 
+          for(cnt=0;cnt<=1200000;cnt++);
+        }  
+  }
+
+void Task2 (void *pvParameters  )  // This is a Task 2.
+  {
+   // prints Tache 2 to the serial monitor
+    volatile uint32_t cnt ;
+
+    for (;;) // A Task shall never return or exit.
+        {       
+          Serial.println(" Tache 2");
+          //delay(500);
+            for(cnt=0;cnt<=1200000;cnt++);
+        }  
+  }
